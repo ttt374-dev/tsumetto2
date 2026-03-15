2f1c3c0cf1f03f0d64812286df8743c428ce26c1
Your Name <you@example.com>
2026-03-15 19:39:44 +0900

    sessionplayercontent

--- src/ui/session/SessionPlayerScreen.tsx
+++ src/ui/session/SessionPlayerScreen.tsx
@@ -1,35 +1,55 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import PlayerScreen from "../player/PlayerScreen"
 import { useSessionPlayerViewModel } from "./hooks/useSessionPlayerViewModel"
 import type { SolvedResult } from "@/domain/learning/entity/Learning"
 import { SolvedDialog } from "../player/dialogs/SolvedDialog"
+import type { Problem } from "@/domain/problem/entity/Problem"
 
 ////////////////////////////////////////////////
 export default function SessionPlayerScreen() {
-    const vm = useSessionPlayerViewModel()
-    const [solvedResult, setSolvedResult] = useState<SolvedResult | undefined>(undefined)
+    const vm = useSessionPlayerViewModel()   
 
     if (vm.status !== "playing") return <>{vm.status}</>
+    return (<SessionPlayerContent 
+        problem={vm.problem}
+        title={vm.title}
+        onSubmitAnswer={vm.submitAnswer}
+        onUndoLastAnswer={vm.undoLastAnswer}
+        onNextProblem={vm.nextProblem}        
+    />)
+}
+
+function SessionPlayerContent(props: {
+    problem: Problem
+    title: string
+    onSubmitAnswer: (res: SolvedResult) => void
+    onUndoLastAnswer: () => void
+    onNextProblem: () => void
+}) {
+    const [solvedResult, setSolvedResult] = useState<SolvedResult | undefined>(undefined)
+
+    useEffect(()=>{
+        setSolvedResult(undefined)
+    }, [props.problem.id])
 
     const handleResolved = (res: SolvedResult) => {
-        vm.submitAnswer(res)
-        //setOpenResolved(true)
+        props.onSubmitAnswer(res)
         setSolvedResult(res)
     }
     
     return (
         <>
-            <PlayerScreen problem={vm.problem}
-                title={vm.title}
+            <PlayerScreen problem={props.problem}
+                title={props.title}
                 onResolved={handleResolved}
-                onUndoLastAnswer={vm.undoLastAnswer}
+                onUndoLastAnswer={props.onUndoLastAnswer}
             />
 
             { solvedResult &&
             <SolvedDialog open={solvedResult !== undefined}
                 onClose={() => setSolvedResult(undefined)}
-                onConfirm={vm.nextProblem}
+                onConfirm={props.onNextProblem}
                 solvedResult={solvedResult}
             />}
         </>
