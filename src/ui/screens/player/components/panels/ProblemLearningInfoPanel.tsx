100a4cc3f8afb9cbe3d3c5ccd391c33fb3f232a5
Your Name <you@example.com>
2026-05-06 17:58:39 +0900

    learnin ginfo panel

--- src/ui/screens/player/components/panels/ProblemLearningInfoPanel.tsx
+++ src/ui/screens/player/components/panels/ProblemLearningInfoPanel.tsx
@@ -2,39 +2,44 @@ import { Box } from "@mui/material"
 
 import type { Problem } from "@/domain/problem/entity/Problem"
 import { toProblemViewData } from "@/ui/features/problem/hooks/problemPresenter"
-import { useLearningRecordStore } from "@/ui/features/learning/hooks/useLearningRecordStore"
 import { toLearningStateViewData } from "@/ui/features/learning/hooks/learningPresenter"
 import type { LearningState } from "@/domain/learning/entity/LearningState"
 
 export default function ProblemLearningInfoPanel(props: {
-    problem: Problem, learningState: LearningState| undefined
+    problem: Problem, learningState: LearningState | undefined
 }) {
-    const { problem } = props
-    //const learningState = useLearningRecordStore(s => s.getState(problem.id))
-    const learningStateVd = props.learningState && toLearningStateViewData(props.learningState)
-    const problemVd = toProblemViewData(problem)
-
+    const { problem, learningState } = props
     return (
         <>
-            <Box>
-                {problemVd.type}{problemVd.plyLength}
-            </Box>
-            {problem.source &&
-                <Box>{problem.source}</Box>}
-            {problem.tags &&
-                <Box>{problemVd.tags}</Box>}
+            <ProblemInfoPanel problem={problem} />
+            {learningState && <LearningInfoPanel learningState={learningState} />}
 
-            {learningStateVd &&
-                <>
-                    <Box>
-                        {learningStateVd.performace}
-                    </Box>
-                    <Box>
-                        {learningStateVd.nextReviewedIn}
-                    </Box>
-                    <Box>
-                        {learningStateVd.masteryStatus}
-                    </Box>
-                </>}
         </>)
+}
+function ProblemInfoPanel({ problem }: { problem: Problem }) {
+    const problemVd = toProblemViewData(problem)
+    return <>
+        <Box>
+            {problemVd.type}{problemVd.plyLength}
+        </Box>
+        {problem.source &&
+            <Box>{problem.source}</Box>}
+        {problem.tags &&
+            <Box>{problemVd.tags}</Box>}
+    </>
+
+}
+function LearningInfoPanel({learningState}: { learningState: LearningState}){
+    const learningStateVd = toLearningStateViewData(learningState)
+    return <>
+        <Box>
+            {learningStateVd.performace}
+        </Box>
+        <Box>
+            {learningStateVd.nextReviewedIn}
+        </Box>
+        <Box>
+            {learningStateVd.masteryStatus}
+        </Box>
+    </>
 }
