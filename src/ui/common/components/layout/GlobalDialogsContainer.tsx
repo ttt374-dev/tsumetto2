5a36faff4c0c181cf8e0be031c853c1778b8f3ef
Your Name <you@example.com>
2026-06-08 15:50:23 +0900

    dialgo container

--- src/ui/common/components/layout/GlobalDialogsContainer.tsx
+++ src/ui/common/components/layout/GlobalDialogsContainer.tsx
@@ -2,32 +2,38 @@ import type { ImportOptions } from "@/application/usecase/ImportProblemsUsecase"
 import { routePaths } from "@/router/paths";
 import type { GlobalDialogControllers } from "@/ui/common/components/layout/AppShell";
 import { ImportDialog } from "@/ui/dialogs/Import/ImportDialog";
+import type { ImportController } from "@/ui/dialogs/Import/useImportController";
 import { createSessionId, useSessionStore } from "@/ui/screens/session/store/useSessionStore";
 import { useNavigate } from "react-router-dom";
 
 
-export function GlobalDialogs({ dialogs }: {
+export function GlobalDialogsContainer({ dialogs }: {
     dialogs: GlobalDialogControllers    
 }) {
+    return <>
+        <GlobalImportContainer importController={dialogs.import}/>
+    </>
+}
+
+function GlobalImportContainer({ importController }: { importController: ImportController }) {
     const navigate = useNavigate()
     const startSession = useSessionStore(s => s.start)
 
     const handleImport = async (options: ImportOptions) => {
-       const result = await dialogs.import.confirm(options)       
-       const ids = result.results.filter(r => r.status === "imported").map(r => r.problemId)
+        const result = await importController.confirm(options)
+        const ids = result.results.filter(r => r.status === "imported").map(r => r.problemId)
         startSession(createSessionId(), ids)
         navigate(routePaths.list, { state: { title: "imported kif files", ids: ids } })
     }
     return <>
-        {dialogs.import.filesSelectElement}
+        {importController.filesSelectElement}
 
-        {dialogs.import.files &&
+        {importController.files &&
             <ImportDialog
-                open={dialogs.import.open}
-                onClose={dialogs.import.cancel}
+                open={importController.open}
+                onClose={importController.cancel}
                 onImport={handleImport}
-                filesToImport={dialogs.import.files}
+                filesToImport={importController.files}
             />}
-
-        </>
+    </>
 }
